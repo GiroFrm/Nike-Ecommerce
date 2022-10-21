d25a760e51a89b52493efb77927f8365ac28d85e
GiroFrm <girofram844@gmail.com>
2022-10-21 14:56:10 +1100

    blur nav bar z-pos fixed

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -37,7 +37,7 @@ const Cart = () => {
   return (
     <>
      <div className={`fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen 
-     opacity-150 z-[250]${ifCartState ? 'opacity-100 visible translate-x-0 ': 'opacity-0 invisible translate-x-8'}`}>
+     opacity-150 z-[280]${ifCartState ? 'opacity-100 visible translate-x-0 ': 'opacity-0 invisible translate-x-8'}`}>
        <div className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}>
         <CartCount totalQTY={totalQTY} onCartToggle={onCartToggle} onClearCartItems={onClearCartItems}/>
         {cartItems?.length === 0 ? <CartEmpty onCartToggle={onCartToggle}/> : <div>
